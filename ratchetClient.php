d8b6afbe99b2c4696ec5dc09c5655b8b1327a8b6
wenewzhang <wenewboy@gmail.com>
2019-01-16 11:59:03 +0800

    receive & reply message

--- ratchetClient.php
+++ ratchetClient.php
@@ -3,6 +3,7 @@
     require __DIR__ . '/vendor/autoload.php';
     use ExinOne\MixinSDK\Traits\MixinSDKTrait;
     use Ramsey\Uuid\Uuid;
+    use Ratchet\RFC6455\Messaging\Frame;
 
     $loop = \React\EventLoop\Factory::create();
     $reactConnector = new \React\Socket\Connector($loop, [
@@ -29,23 +30,32 @@
                                           ])
     ->then(function(Ratchet\Client\WebSocket $conn) {
         $conn->on('message', function(\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn) {
-            $jsMsg = gzdecode($msg);
-            echo "Received: {$jsMsg}\n";
-            // $conn->close();
-            // echo gzdecode($msg);
+            $jsMsg = json_decode(gzdecode($msg));
+            if ($jsMsg->action === 'CREATE_MESSAGE') {
+              echo "\nNeed reply server a receipt!\n";
+              $IncomingMsg = ["message_id" => $jsMsg->data->message_id, "status" => "READ"];
+      	      $RspMsg = ["id" => Uuid::uuid4()->toString(), "action" => "ACKNOWLEDGE_MESSAGE_RECEIPT",
+                          "params" => $IncomingMsg];
+              $msg = new Frame(gzencode(json_encode($RspMsg)),true,Frame::OP_BINARY);
+              $conn->send($msg);
+              if ($jsMsg->data->category === 'PLAIN_TEXT') {
+                  echo "PLAIN_TEXT:".base64_decode($jsMsg->data->data);
+              }
+            }
         });
 
         $conn->on('close', function($code = null, $reason = null) {
             echo "Connection closed ({$code} - {$reason})\n";
         });
+/*                   start listen for the incoming message          */
         $message = [
             'id'     => Uuid::uuid4()->toString(),
             'action' => 'LIST_PENDING_MESSAGES',
         ];
-        print_r($message);
-        $jsMsg = json_encode($message);
-        echo "Send: {$jsMsg}\n";
-        $conn->send(gzencode($jsMsg));
+        print_r(json_encode($message));
+        $msg = new Frame(gzencode(json_encode($message)),true,Frame::OP_BINARY);
+        $conn->send($msg);
+        // $conn->send(gzencode($msg,1,FORCE_DEFLATE));
     }, function(\Exception $e) use ($loop) {
         echo "Could not connect: {$e->getMessage()}\n";
         $loop->stop();
