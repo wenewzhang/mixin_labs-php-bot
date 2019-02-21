beb6ebffccd384cb115b4413c69547b4c99ff2ea
wenewzhang <wenewboy@gmail.com>
2019-02-21 10:54:15 +0800

    config example

---
+++ config-example.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'mixin_id'      => '7000101716',
+    'client_id'     => 'a1ce2967-a534-417d-bf12-c86571e4eefa',
+    'client_secret' => 'f8e0241bc76461a6032a7a80f9c8b9b10303308cbf772b218bbad2a819c62556',
+    'pin'           => '806669',
+    'pin_token'     => 'H8U9BJly4EjN5d/LPUjjXbgHOJDzqyHHa/Os9hT6g5P06Xl475XlsL7KB0Lj15jpBL3SLlgDEiz3+drchsX8fPfEeido8hgnmZJJRkAW+S2p5AdTShWQmpi03fdbkK4ARqOpBIRXu9l8RJl17I4N4ZNsx09O2p2EfbQDDjaVDVg=',
+    'session_id'    => 'fe4ff62a-ce5a-4db3-9f53-3f365a260541',
+    'private_key'   => <<<EOF
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCEvNjdA/Vk/yn2l0fHU/+IvOp8iYWnIEl//tGRS0tLFjF/Dtm5
++a6PtEx33nhGuiyLA4kDEBIEECOfD5nLy099CRzKiVjMbl1b11M/oNm1MJdmWjSg
+umKrvBCac8oLPLdnb+xnqTZofrF84UbZjuZ4EGMyEs5Daw0NUPE1tJaA2wIDAQAB
+AoGAd3cgF4tAiS2+LtnpGFVZX0/oGz4KtGgyvqgxJXuGwIWw9pH/E4rMuTmkuR3Z
+Uo6tGFCON9t224FLDhWUbf8GAk5Uj/+WXiWVKmHsKtn93axkRhkxuXrv+REWOE6J
+6XdGb5K6Fx2ud2HZ1L/CEADCoDLCq4OjFG7ey4c/fIycpkECQQDFzEsTQgogMYlX
+iBQMP6l075AQuhC5o1VKlOgO1/7Irbq1M4P2GxQNFGVvyUE74yQSM/FAhCCHfac3
+1wLbvYuXAkEAq8uyPZHbFEbEIOgbaImN/1qJY+/oqSyNriT1APkqnvQnDCLrlRVV
++FyV5C4eeZyVa00WFj7IOSJUMLwEnYDtXQJBAI449eZz/rnlRH7Wzqt7/wmg07Lj
+RvFkOwi0hyNdNcrv+CcgUotcLw+0kbdOO4SnLyGTja25E3a458qj5F9CLCMCQCTE
+YY8/yg1a39rTEhqbZeKCs+jJjZe3S1M74Zult/Nw+XJlftnXSSDwX7wICsmoM2pV
+gyabpSplKHONqcczspkCQCDsmsgMP5ETBEDY1tqpa4YEgGB/rzizFuPWlzHm8OzA
+g5w07wY/SeuA4BqDoNIycGK9OewtRlcbJmLLpx3F/DM=
+-----END RSA PRIVATE KEY-----
+EOF
+    ,  //import your private_key
+];
