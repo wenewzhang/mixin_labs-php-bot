4e085a9094d4e6274906897535cbf591467b3866
Li lin <yjh521@gmail.com>
2019-02-21 10:10:50 +0800

    Update createUser.php

--- createUser.php
+++ createUser.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 use ExinOne\MixinSDK\MixinSDK;
-$mixinSdk = new MixinSDK(require './config.php');
+$bot_instance = new MixinSDK(require './config.php');
 
 const PIN           = "945689";
 const MASTER_ID     = "37222956";//Developer Mixin messenger account ID
@@ -10,7 +10,7 @@ const BTC_ASSET_ID  = "c6d0c728-2624-429b-8e0d-d9d19b6592fa";
 const AMOUNT        = "0.001";
 
 //Mixin PHP SDK generate RSA key pair automatically
-$user_info = $mixinSdk->Network()->createUser("Tom cat");
+$user_info = $bot_instance->Network()->createUser("Tom cat");
 //Create User api include all account information
 print_r($user_info);
 print($user_info["pubKey"]);
@@ -22,26 +22,26 @@ $newConfig["session_id"]  = $user_info["session_id"];
 $newConfig["client_id"]   = $user_info["user_id"];
 $newConfig["pin"]         = PIN;
 //Install the parameter in PHP SDK
-$mixinSdkNew = new MixinSDK($newConfig);
+$account_instance = new MixinSDK($newConfig);
 
 //Create a PIN.
-$pinInfo = $mixinSdkNew->Pin()->updatePin('',PIN);
+$pinInfo = $account_instance->Pin()->updatePin('',PIN);
 print_r($pinInfo);
 print_r($trans_info);
 //Create Bitcoin deposit address by read Bitcoin asset
-$asset_infoNew = $mixinSdkNew->Wallet()->readAsset(BTC_ASSET_ID);
+$asset_infoNew = $account_instance->Wallet()->readAsset(BTC_ASSET_ID);
 print_r("BitCoin wallet address is :".$asset_infoNew["public_key"]);
 
 //Transfer Bitcoin to developer's account, zero fee and confirmed instantly
-$trans_info = $mixinSdk->Wallet()->transfer(BTC_ASSET_ID,$newConfig["client_id"],
-                                         $mixinSdk->getConfig()['default']['pin'],AMOUNT);
+$trans_info = $bot_instance->Wallet()->transfer(BTC_ASSET_ID,$newConfig["client_id"],
+                                         $bot_instance->getConfig()['default']['pin'],AMOUNT);
 print_r($trans_info);
 
 //Read user information by ID
-$userInfo = $mixinSdk->Network()->readUser(MASTER_ID);
+$userInfo = $bot_instance->Network()->readUser(MASTER_ID);
 $userInfo["user_id"];
 
-$trans_info2 = $mixinSdkNew->Wallet()->transfer(BTC_ASSET_ID,$userInfo["user_id"],
-                                         $mixinSdkNew->getConfig()['default']['pin'],AMOUNT);
+$trans_info2 = $account_instance->Wallet()->transfer(BTC_ASSET_ID,$userInfo["user_id"],
+                                         $account_instance->getConfig()['default']['pin'],AMOUNT);
 
 print_r($trans_info2);
