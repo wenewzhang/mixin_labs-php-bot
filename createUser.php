f3391bc03f68274197b0061f6586c9848d92ac68
Li lin <yjh521@gmail.com>
2019-02-21 10:17:18 +0800

    Update createUser.php

--- createUser.php
+++ createUser.php
@@ -10,30 +10,30 @@ const BTC_ASSET_ID  = "c6d0c728-2624-429b-8e0d-d9d19b6592fa";
 const AMOUNT        = "0.001";
 
 //Mixin PHP SDK generate RSA key pair automatically
-$tom_cat_info = $bot_instance->Network()->createUser("Tom cat");
+$tomcat_info = $bot_instance->Network()->createUser("Tom cat");
 //Create User api include all account information
-print_r($tom_cat_info);
-print($tom_cat_info["pubKey"]);
+print_r($tomcat_info);
+print($tomcat_info["pubKey"]);
 
 $newConfig = array();
-$newConfig["private_key"] = $tom_cat_info["priKey"];
-$newConfig["pin_token"]   = $tom_cat_info["pin_token"];
-$newConfig["session_id"]  = $tom_cat_info["session_id"];
-$newConfig["client_id"]   = $tom_cat_info["user_id"];
+$newConfig["private_key"] = $tomcat_info["priKey"];
+$newConfig["pin_token"]   = $tomcat_info["pin_token"];
+$newConfig["session_id"]  = $tomcat_info["session_id"];
+$newConfig["client_id"]   = $tomcat_info["user_id"];
 $newConfig["pin"]         = PIN;
 //Install the parameter in PHP SDK
-$tom_cat = new MixinSDK($newConfig);
+$tomcat_instance = new MixinSDK($newConfig);
 
 //Create a PIN.
-$pinInfo = $tom_cat->Pin()->updatePin('',PIN);
+$pinInfo = $tomcat_instance->Pin()->updatePin('',PIN);
 print_r($pinInfo);
 print_r($trans_info);
 //Create Bitcoin deposit address by read Bitcoin asset
-$asset_infoNew = $account_instance->Wallet()->readAsset(BTC_ASSET_ID);
+$asset_infoNew = $tomcat_instance->Wallet()->readAsset(BTC_ASSET_ID);
 print_r("BitCoin wallet address is :".$asset_infoNew["public_key"]);
 
 //Transfer Bitcoin to developer's account, zero fee and confirmed instantly
-$trans_info = $bot_instance->Wallet()->transfer(BTC_ASSET_ID,$$tom_cat_info["client_id"],
+$trans_info = $bot_instance->Wallet()->transfer(BTC_ASSET_ID,$tomcat_info["client_id"],
                                          $bot_instance->getConfig()['default']['pin'],AMOUNT);
 print_r($trans_info);
 
@@ -41,7 +41,7 @@ print_r($trans_info);
 $master_userInfo = $bot_instance->Network()->readUser(MASTER_ID);
 $master_userInfo["user_id"];
 
-$trans_info2 = $tom_cat->Wallet()->transfer(BTC_ASSET_ID,$master_userInfo["user_id"],
+$trans_info2 = $tomcat_instance->Wallet()->transfer(BTC_ASSET_ID,$master_userInfo["user_id"],
                                          $account_instance->getConfig()['default']['pin'],AMOUNT);
 
 print_r($trans_info2);
